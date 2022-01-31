749aa4704f0d4e55415524d2432c76dd34ab3573
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-31 12:04:42 -0800

    exercise 4

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,10 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+
+SELECT customer.first_name, customer.last_name, address.address, city.city
+FROM customer
+INNER JOIN address
+ON address.address_id = customer.address_id
+INNER JOIN city
+ON city.city_id = address.city_id
+;
